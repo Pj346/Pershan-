4ac19b096233558450d06e3da070b6716ecd8b8f
Pj346 <jazeyerifarpershan@gmail.com>
2026-08-23 10:18:20 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-
+implementation("androidx.compose.material:material-icons-extended")
     implementation(platform("androidx.compose:compose-bom:2025.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
